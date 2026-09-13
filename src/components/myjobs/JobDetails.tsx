89e509eacde2dfcc6c3e21b75ca9fcf57ca19ce1
Khuram Niaz <knshaikh@gmail.com>
2026-09-13 05:41:06 -0600

    feat(jobs): hide match score for unanalyzed jobs and update related components
    (cherry picked from commit 11e72f362c6d93accc42420b21896e3873570dba)

--- src/components/myjobs/JobDetails.tsx
+++ src/components/myjobs/JobDetails.tsx
@@ -235,7 +235,7 @@ function JobDetails({
           </TabsContent>
           <TabsContent value="match" className="mt-4">
             <Card className="p-6">
-              {parsedMatchData ? (
+              {parsedMatchData && parsedMatchData.analyzed !== false ? (
                 <MatchDetails matchData={parsedMatchData} />
               ) : (
                 <JobTabEmptyState
